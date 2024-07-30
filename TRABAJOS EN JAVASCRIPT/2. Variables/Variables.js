12fd4fe0c6b204bc80735fca0cfc09cdc92ca34c
crdz21 <119645129+crdz21@users.noreply.github.com>
2024-07-30 14:06:33 -0600

    Add files via upload

---
+++ TRABAJOS EN JAVASCRIPT/2. Variables/Variables.js
@@ -0,0 +1,18 @@
+/* JavaScript trabaja principalmente con dos tipos de variables, que pueden comenzar con con VAR y o con LET*/
+
+// <- Usar doble slash(/) solo funciona para poner comentarios en UNA SOLA LÍNEA, para comentar en más de una se usan otras tipologías.
+
+
+
+/* Con var, las variables se vuelven GLOBALES, usables para todo el archivo con el mismo valor. */
+
+var nombre = "Mateo";
+
+
+
+// Al contrario que las varuables VAR, con LET se definen variables LOCALES, solo usables en un bloque, no son repetibles.
+
+let Código;
+
+
+document.write(nombre);
---
+++ TRABAJOS EN JAVASCRIPT/3. Tipos de datos/Tipos de datos.js
@@ -0,0 +1,9 @@
+// Tipos de Variables
+
+
+
+var numéricos = 5.25;
+var Texto = "Hola";
+var Booleanos = true;
+
+document.write(numéricos, "<br>", Texto, "<br>", Booleanos)
